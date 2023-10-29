661c8764ebc498549db26e9cf8e6832823d3d578
ahadhalloul <ahadhalloul@hotmail.com>
2023-10-29 16:51:16 +0300

    adding al level on portfolio section

---
+++ main.js
@@ -0,0 +1,17 @@
+// let advancedDetailArrow = document.getElementById("hideShowAdvancedDetail");
+let advancedDetail = document.getElementById("portfolio-assets");
+let showArrow = document.getElementById("showArrow");
+function hideShowAdvancedDetail() {
+  advancedDetail.classList.toggle("portfolio-assets-hide");
+  showArrow.classList.toggle("show-arrow");
+}
+
+// let tickerTable = document.getElementById("ticker-data");
+// let tickerRow = document.createElement("tr");
+// let tickerData = document.createElement("td");
+// function addTicker() {
+//   tickerRow.appendChild(tickerData);
+//   tickerTable.appendChild(tickerRow);
+//   console.log(tickerRow);
+//   console.log(tickerTable);
+// }
