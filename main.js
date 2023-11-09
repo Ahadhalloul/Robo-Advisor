5bf6b87c64f1115401ac0f5f7607c26054991635
ahadhalloul <ahadhalloul@hotmail.com>
2023-11-09 23:53:40 +0300

    up/down arrow toggle

--- main.js
+++ main.js
@@ -19,7 +19,21 @@ function hideShowAdvancedDetail() {
 let portfolioWeightDistributionCard = document.getElementById(
   "portfolio-weight-distribution-card"
 );
-let arrow2 = document.getElementById("weights-distribution-arrow");
-function showsection() {
+
+let showSectionArrow = document.getElementById(
+  "weights-distribution-arrow-down"
+);
+
+let hideSectionArrow = document.getElementById("weights-distribution-arrow-up");
+
+function showSection() {
   portfolioWeightDistributionCard.classList.toggle("show");
+  showSectionArrow.classList.toggle("hide");
+  hideSectionArrow.classList.toggle("hide");
+}
+
+function hideSection() {
+  portfolioWeightDistributionCard.classList.toggle("show");
+  showSectionArrow.classList.toggle("hide");
+  hideSectionArrow.classList.toggle("hide");
 }
